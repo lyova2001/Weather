2ead49b57539248f49d2dbe294cf6ffc0bdf3e95
Leva Avetisian <lyovawork@gmail.com>
2022-01-26 13:51:15 +0300

    Styles for main layout

--- src/common/header/index.tsx
+++ src/common/header/index.tsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import BottomNavigation from '@mui/material/BottomNavigation'
+import BottomNavigationAction from '@mui/material/BottomNavigationAction'
+
+import './style.scss'
+import { AppBar, Toolbar } from '@mui/material'
 
 export default function Header() {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
   return (
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="/">Home</Link> | <Link to="/day">Day</Link> |
-        <Link to="/week">Week</Link> |
-        <Link to="/month">Month</Link>
-      </nav>
-  );
+    <AppBar sx={{ background: '#333' }} position='static'>
+      <Toolbar>
+        <BottomNavigation
+          showLabels
+          value={pathname}
+          onChange={(event, newValue) => {
+            navigate(newValue)
+          }}
+          sx={{ background: 'transparent' }}
+        >
+          <BottomNavigationAction value='/' label='Home' />
+          <BottomNavigationAction value='/day' label='Day' />
+          <BottomNavigationAction value='/week' label='Week' />
+          <BottomNavigationAction value='/month' label='Month' />
+        </BottomNavigation>
+      </Toolbar>
+    </AppBar>
+  )
 }
--- src/common/layout/index.tsx
+++ src/common/layout/index.tsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import { Counter } from "../../features/counter/Counter";
-import DayForecast from "../../pages/day";
-import MonthForecast from "../../pages/month";
-import WeekForecast from "../../pages/week";
-import Header from "../header";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Counter } from '../../features/counter/Counter'
+import DayForecast from '../../pages/day'
+import MonthForecast from '../../pages/month'
+import WeekForecast from '../../pages/week'
+import Header from '../header'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Container from '@mui/material/Container'
 
 export default function LayoutWrapper() {
   return (
     <>
       <CssBaseline />
       <Container fixed>
-        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }}>
+        <Box sx={{ bgcolor: '#fff', height: '100vh' }}>
           <Header />
           <Routes>
-            <Route path="/" element={<Counter />}>
-              <Route path="day" element={<DayForecast />} />
-              <Route path="week" element={<WeekForecast />} />
-              <Route path="month" element={<MonthForecast />} />
+            <Route path='/' element={<Counter />}>
+              <Route path='day' element={<DayForecast />} />
+              <Route path='week' element={<WeekForecast />} />
+              <Route path='month' element={<MonthForecast />} />
             </Route>
           </Routes>
         </Box>
       </Container>
     </>
-  );
+  )
 }
